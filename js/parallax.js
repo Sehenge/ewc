a70e678463706e6812cb3434a56de452527d41ea
sehenge <sehenge@gmail.com>
2013-04-15 17:52:27 +0300

    Alpha parallax

--- js/parallax.js
+++ js/parallax.js
@@ -14,22 +14,35 @@ Parallax.prototype.InitEvents = function Parallax_initEvents() {
     var self = this;
 
     this.navli.click(function(){
-        if (!jQuery(this).hasClass('search')) {
-            jQuery(this).addClass('active');
-            if (!jQuery(this).children(".blue_border").length) {
-                jQuery(this).append(this.blueborder);
-            }
-            jQuery(this).siblings().removeClass('active').children(".blue_border").remove();
-        }
+        self.ScrollTo(jQuery(this));
     });
 }
 
 Parallax.prototype.OnScroll = function Parallax_onScroll(obj) {
+    var self = this;
     if (!obj.hasClass('search')) {
         obj.addClass('active');
         if (!obj.children(".blue_border").length) {
-            obj.append(this.blueborder);
+            obj.append(self.blueborder);
         }
         obj.siblings().removeClass('active').children(".blue_border").remove();
     }
+}
+
+Parallax.prototype.ScrollTo = function Parallax_scrollTo(obj) {
+    var self = this;
+    var block = obj.find('a').attr("rel");
+    var offset = 0;
+    if (block == 'men') {
+        offset = 974;
+    } else if (block == 'designers') {
+        offset = 1948;
+    } else if (block == 'sales') {
+        offset = 2923;
+    } else if (block == 'bestsellers') {
+        offset = 3896;
+    }
+    $("body").animate({
+        scrollTop: offset
+    }, 800);
 }
